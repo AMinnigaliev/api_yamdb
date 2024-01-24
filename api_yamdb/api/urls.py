5ab65a174083a62dd03fd0cbb1cc99a27af6dccb
Artem Minnigaliev <artyomminnigaliev@gmail.com>
2024-01-24 14:04:23 -0800

    Correction of reviewer comments

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,10 +6,10 @@ from api.views import CategoryViewSet, GenreViewSet
 app_name = 'api'
 
 
-router = DefaultRouter()
-router.register('categories', CategoryViewSet, basename='category')
-router.register('genres', GenreViewSet, basename='genre')
+router_v1 = DefaultRouter()
+router_v1.register('categories', CategoryViewSet, basename='category')
+router_v1.register('genres', GenreViewSet, basename='genre')
 
 urlpatterns = [
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,29 +1,34 @@
-from rest_framework import viewsets, mixins
+from rest_framework import filters, viewsets, mixins
 
 from api.permissions import IsAdminUserOrReadOnly
 from api.serializers import GenreSerializer, CategorySerializer
 from reviews.models import Genre, Category
 
 
-class GenreViewSet(mixins.CreateModelMixin,
+class GenreCategoryViewMixin:
+
+    lookup_field = 'slug'
+    permission_classes = (IsAdminUserOrReadOnly,)
+    http_method_names = ['get', 'post', 'delete']
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=slug',)
+
+
+class GenreViewSet(GenreCategoryViewMixin,
+                   mixins.CreateModelMixin,
                    mixins.DestroyModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet,):
 
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    lookup_field = 'slug'
-    permission_classes = (IsAdminUserOrReadOnly,)
-    http_method_names = ['get', 'post', 'delete']
 
 
-class CategoryViewSet(mixins.CreateModelMixin,
+class CategoryViewSet(GenreCategoryViewMixin,
+                      mixins.CreateModelMixin,
                       mixins.DestroyModelMixin,
                       mixins.ListModelMixin,
                       viewsets.GenericViewSet,):
 
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    lookup_field = 'slug'
-    permission_classes = (IsAdminUserOrReadOnly,)
-    http_method_names = ['get', 'post', 'delete']
